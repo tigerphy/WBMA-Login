4fc6780e57e929a5b6626d6c9533265f857ba264
Chingman <chingman.lai@metropolia.fi>
2021-01-23 15:18:33 +0200

    first commit

---
+++ views/Profile.js
@@ -0,0 +1,40 @@
+import React, {useContext} from 'react';
+import {StyleSheet, SafeAreaView, Text, Button} from 'react-native';
+import PropTypes from 'prop-types';
+import {MainContext} from '../contexts/MainContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const Profile = ({navigation}) => {
+  const [isLoggedIn, setIsLoggedIn] = useContext(MainContext);
+  console.log('profile', isLoggedIn);
+  const logout = async () => {
+    setIsLoggedIn(false);
+    await AsyncStorage.clear();
+    if (!isLoggedIn) {
+      // this is to make sure isLoggedIn has changed, will be removed later
+      navigation.navigate('Login');
+    }
+  };
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Profile</Text>
+      <Button title={'Logout'} onPress={logout} />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 40,
+  },
+});
+
+Profile.propTypes = {
+  navigation: PropTypes.object,
+};
+
+export default Profile;
